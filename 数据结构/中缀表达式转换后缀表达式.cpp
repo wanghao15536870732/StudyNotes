deac2a6949f13eb237498f04795982ce8cdd917f
wanghao15536870732 <1484290617@qq.com>
2018-09-19 22:11:48 +0800

    add android notes.

---
+++ 数据结构/中缀表达式转换后缀表达式.cpp
@@ -0,0 +1,97 @@
+#include "stdio.h"
+
+int is_operation(char op);
+int priority(char op);
+void postfix(char e[],char f[]);
+
+int main()
+{
+	char a[10] = "a*(b+c)-a";
+	char b[10] = {0};
+	postfix(a,b);
+	for(int i = 0;i < 10;i ++)
+	{
+		printf("%c",b[i]);
+	}
+	printf("\n");
+	return 0;
+}
+
+/****************************************/
+/****** 判断一个字符串是否为运算符 ******/
+/****************************************/
+int is_operation(char op)
+{
+	switch(op)
+	{
+		case '+':
+		case '-':
+		case '*':
+		case '-/': return 1;
+		default:return 0;
+	}
+}
+
+/****************************************/
+/*********** 求运算符的优先级 ***********/
+/****************************************/
+int priority(char op)
+{
+	switch(op)
+	{
+		case '#': return -1;
+		case '(': return 0;
+		case '+': 
+		case '-': return 1;
+		case '*':
+		case '/': return 2;
+		default: return -1;
+	}
+}
+
+
+/***************************************************/
+/* 将一个中缀表达式 e 转换成与它等价的后缀表达式 f */
+/***************************************************/
+
+void postfix(char e[],char f[])
+{
+	int i = 0,j = 0;
+	char opst[100];
+	int top,t;
+	top = 0;
+	opst[top] = '#'; 
+	top ++;
+	while(e[i])
+	{
+		if((e[i] >= '0' && e[i] <= '9') || e[i] == '.')
+			f[j ++] = e[i];  // 遇到数字或者小鼠点直接写入后缀表达式
+		else if( e[i] == '(')  // 遇到左括号进入操作符栈
+		{
+			opst[top] == e[i]; 
+			top ++;
+		}
+		else if(e[i] == ')')
+			//遇到右括号将其对应的左括号后的操作符全部写入后缀表达式 
+		{
+			t = top - 1;
+			while(opst[t] != '(')
+			{
+				f[j ++] = opst[-- top];
+				t = top - 1;
+			}
+			top --; // '( ' 出栈
+		}
+		else if(is_operation(e[i])) // '+ 、-、*、/ '
+		{
+			f[j ++] = ' '; // 用空格分开两个操作符
+			while(priority(opst[top - 1]) >= priority(e[i]))
+				f[j ++] = opst[-- top];
+			opst[top] = e[i];
+			top ++;// 当前元素入栈
+		}
+		i ++;// 处理下一个元素
+	}
+	while(top) 
+		f[j ++] = opst[-- top];
+}
