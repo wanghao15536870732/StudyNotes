a00297dcb93652d9002616e8669cb6612f95654d
wanghao15536870732 <1484290617@qq.com>
2018-09-29 22:31:41 +0800

    add other test.

--- 数据结构/课后习题/习题3.7.cpp
+++ 数据结构/课后习题/习题3.7.cpp
@@ -1,10 +1,6 @@
-/*
-习题 3. 7 将一个结点值为自然数得单链表拆分为两个单链表，
-原表当中保留值为偶数的结点，而值为奇数的结点按他们在元表中的
-相对次序组成一个新的链表
-*/
 #include "stdio.h"
 #include "stdlib.h"
+
 /**************************/
 /*****链表表现头文件*******/
 /**************************/
@@ -24,7 +20,9 @@ node *my_scanf(node *head);
 //提取奇数
 node *separate(node *head1,node *head2);
 //剩余偶数的旧连表
-node *surplus(node *head,node *head2);
+node *surplus(node *head);
+/****单链表中删除一个值为x的结点******/
+node *dele(node *head,datatype x);
 
 int main()
 {
@@ -39,9 +37,9 @@ int main()
 	head1 = separate(head,head1);
 	printf("\n提取奇数节点的新链表：\n");
 	display(head1);
-	head1 = surplus(head,head1);
+	head = surplus(head);
 	printf("\n剩余偶结点的旧链表：\n");
-	display(head1);
+	display(head);
 	return 0;
 }
 
@@ -137,32 +135,55 @@ node *separate(node *head1,node *head2)
 /**************************/
 /****剩余偶数的旧连表******/
 /**************************/
-node *surplus(node *head,node *head2)
+node *surplus(node *head)
 {
-	node *p = head,*q = NULL,*m = NULL;
-	if(p->next == NULL)
-		printf("链表是空的!\n");
-	int a = 0;
+	node *p = head,*pre = NULL;
+	if(!head)
+	{
+		printf("单链表是空的！");
+		return head;
+	}
+	pre = head;
+	p = head->next;
 	while(p)
 	{
-		if(p->info % 2 == 0)
-		{
-			q = (node *)malloc(sizeof(node));
-			q->info = p->info;
-			q->next = NULL;
-			if(a == 0)
-			{
-				head2 = q;
-				m = q;
-				a = 1;
-			}
-			else
-			{
-				m->next = q;
-				m = q;
-			}
-		}
+		if(pre == head && pre->info % 2 != 0)
+			head = head->next;
+		if(p != head && p->info % 2 != 0)
+			pre->next = p->next;
+		pre = p;
 		p = p->next;
 	}
-	return head2;
+	return head;
+}
+
+/**************************************/
+/****单链表中删除一个值为x的结点******/
+/*************************************/
+
+node *dele(node *head,datatype x)
+{
+	node *pre = NULL,*p;
+	if(!head)
+	{
+		printf("单链表是空的！");
+		return head;
+	}
+	p = head;
+	//指针指向数据存在并且没有找到符合的数据
+	while(p && p->info != x)
+	{
+		pre = p;
+		p = p->next;
+	}
+	if(p)
+	{
+		//前驱结点不存在，即想要删除第一个数据
+		if(!pre)
+			head = head->next;
+		else
+			pre->next = p->next;
+		free(p);
+	}
+	return head;
 }
