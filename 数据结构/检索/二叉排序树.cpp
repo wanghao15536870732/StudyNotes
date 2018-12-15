93a9d65e1694f75261df3736828bef2b06216381
wanghao15536870732 <1484290617@qq.com>
2018-12-15 20:20:37 +0800

    :memo: add ++tree.

--- 数据结构/检索/二叉排序树.cpp
+++ 数据结构/检索/二叉排序树.cpp
@@ -3,32 +3,33 @@
 
 typedef struct node
 {
-	int key;  //二叉树节点定义
-	struct node *lchild, *rchild;  //左子树，右子树 
-}bsnode;
+	int key;					  //二叉树节点定义
+	struct node *lchild, *rchild; //左子树，右子树
+} bsnode;
 
-void InsertBstree(bsnode *t, int x)
+bsnode *InsertBstree(bsnode *t, int x)
 {
+	bsnode *root = t;
 	bsnode *f = NULL, *p;
-	p = t;
-	while (p)  //查找插入位置
+	p = root;
+	while (p) //查找插入位置
 	{
 		if (x == p->key) //如果二叉排序树t中已有key,则无需插入
-			return;  
-		f = p;  //用于保存新结点的最终结点的插入位置
+			return root;
+		f = p; //用于保存新结点的最终结点的插入位置
 		p = (x < p->key) ? p->lchild : p->rchild;
 	}
 	p = (bsnode *)malloc(sizeof(bsnode)); //生成待插入节点的新节点
 	p->key = x;
-	p->lchild = p->rchild = NULL;  
-	if (t == NULL)
-		t = p;  //原树为空
+	p->lchild = p->rchild = NULL;
+	if (root == NULL)
+		root = p; //原树为空
 	else if (x < f->key)
 		f->lchild = p;
 	else
 		f->rchild = p;
+	return root;
 }
-
 
 bsnode *CreateBstree()
 {
@@ -38,7 +39,7 @@ bsnode *CreateBstree()
 	scanf("%d", &key);
 	while (key != -1)
 	{
-		InsertBstree(t, key);
+		t = InsertBstree(t, key);
 		scanf("%d", &key);
 	}
 	return t;
@@ -73,13 +74,13 @@ void bssearch1(bsnode *t, int c, bsnode *f, bsnode *p)
 //删除二叉排序树中的任意节点，删除后仍为二叉排序树
 bsnode *DelBstree(bsnode *t, int x)
 {
-	bsnode *p, *q, *child;;
-	bssearch1(t, x, p, q);//找到被删结点
-	if (q)  //找到了待删结结点
+	bsnode *p = NULL, *q = NULL, *child = NULL;
+	bssearch1(t, x, p, q); //找到被删结点
+	if (q)				   //找到了待删结结点
 	{
-		if (q->lchild == NULL && q->lchild == NULL)  //情况一：被删除节点为叶子节点 
+		if (q->lchild == NULL && q->lchild == NULL) //情况一：被删除节点为叶子节点
 		{
-			if (p)  //待删除结点有双亲
+			if (p) //待删除结点有双亲
 			{
 				if (p->lchild == q)
 					p->lchild = NULL;
@@ -87,31 +88,31 @@ bsnode *DelBstree(bsnode *t, int x)
 					p->rchild = NULL;
 			}
 			else
-				t = NULL;  //被删除结点为树根
+				t = NULL; //被删除结点为树根
 			free(q);
 		}
 	}
-	else if (q->rchild == NULL)  //情况2：被删除结点的右子树为空，用被删除结点的左子树替代该节点
+	else if (q->rchild == NULL) //情况2：被删除结点的右子树为空，用被删除结点的左子树替代该节点
 	{
 		if (p)
 		{
 			if (p->lchild == q)
 				p->lchild = q->lchild; //q是双亲结点的左儿子
 			else
-				p->rchild = q->lchild;    //q是双亲结点的右儿子
+				p->rchild = q->lchild; //q是双亲结点的右儿子
 		}
 		else
 			t = q->lchild;
 		free(q);
 	}
-	else if (q->lchild == NULL)  // 情况3：被删除结点的左子树为空，用被删除结点的右子树代替该节点
+	else if (q->lchild == NULL) // 情况3：被删除结点的左子树为空，用被删除结点的右子树代替该节点
 	{
-		if (p)  //被删除结点的双亲结点不为空
+		if (p) //被删除结点的双亲结点不为空
 		{
 			if (p->lchild == q)
-				p->lchild = q->rchild;  //q是双亲结点的左儿子
+				p->lchild = q->rchild; //q是双亲结点的左儿子
 			else
-				p->rchild = q->rchild;  //q是双亲结点的右儿子
+				p->rchild = q->rchild; //q是双亲结点的右儿子
 		}
 		else
 			t = q->rchild;
@@ -120,9 +121,9 @@ bsnode *DelBstree(bsnode *t, int x)
 	else //情况4：被删除结点的左右子树均不为空，用右子树代替被删除的结点，同时将被删除结点的左子树收为右子树中序遍历一开始的那个左儿子
 	{
 		child = q->rchild;
-		while (child->lchild)  //找被删除结点的右子树中序遍历时的左儿子
+		while (child->lchild) //找被删除结点的右子树中序遍历时的左儿子
 			child = child->lchild;
-		child->lchild = q->lchild;  //将被删除结点的左子树收为child的左孩子
+		child->lchild = q->lchild; //将被删除结点的左子树收为child的左孩子
 		if (p)
 		{
 			if (p->lchild == q)
@@ -131,7 +132,7 @@ bsnode *DelBstree(bsnode *t, int x)
 				p->rchild = q->rchild;
 		}
 		else
-			t = q->rchild;  //被删除节点为树根
+			t = q->rchild; //被删除节点为树根
 		free(q);
 	}
 	return t;
