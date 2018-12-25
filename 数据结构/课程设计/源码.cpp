0c94e130c9a8b65131c2e1c6ddc049ed55c09bfc
wanghao15536870732 <1484290617@qq.com>
2018-12-25 08:47:28 +0800

    :memo: add notes.

--- 数据结构/课程设计/源码.cpp
+++ 数据结构/课程设计/源码.cpp
@@ -1,7 +1,26 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-#include "textstruct.h"
+
+#define M 20 /*预定最大的行号*/
+//每个字的结构体
+typedef struct node
+{
+    char data[1000]; //一行字的数据域
+    int length;      // 一行字的数据域
+} Word;
+
+typedef struct vnode
+{
+    int row;         //储存行号
+    Word *FirstWord; //邻接表表头指针,指向每一行的第一个字
+} Vertexnode;
+
+typedef struct
+{                         //邻接表类型
+    Vertexnode column[M]; //存放每一行头节点的顺序表
+    int count;            //记录总行数
+} TextCompiler;           //整个文本编译器结构体
 
 void ReadFromFile(TextCompiler *text, char *filename)
 {
