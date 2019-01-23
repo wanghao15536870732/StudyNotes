631cfeef79bf8876359056ab494eb3d47c98802a
wanghao15536870732 <1484290617@qq.com>
2019-01-23 22:52:10 +0800

    :memo: add notes.

--- Flutter/flutter_lake/lib/FlutterWeather/weather.dart
+++ Flutter/flutter_lake/lib/FlutterWeather/weather.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_lake/FlutterWeather/WeatherData.dart';
 
 class Weather extends StatelessWidget{
   @override
@@ -8,7 +9,7 @@ class Weather extends StatelessWidget{
     return new MaterialApp(
       title: 'Flutter Weather',
       theme: new ThemeData(
-        primarySwatch: Colors.pink
+        primarySwatch: Colors.blue
       ),
       home: new Center(
         child: new WeatherPage(),
@@ -24,6 +25,33 @@ class WeatherPage extends StatefulWidget{
 }
 
 class _WeatherPage extends State<WeatherPage>{
+
+
+  WeatherData weather = WeatherData.empty();
+
+  WeatherState(){
+    _getWeather();
+  }
+
+  void _getWeather() async{
+    WeatherData data = await _fetchWeather();
+    setState((){
+      weather = data;
+    });
+  }
+
+  Future<WeatherData> _fetchWeather() async{
+    final response = await http.get(
+        'https://free-api.heweather.com/s6/weather/now?location=邢台&key=551f547c64b24816acfed8471215cd0e'
+    );
+    if(response.statusCode == 200){
+      return WeatherData.fromJson(json.decode(response.body));
+    }else{
+      return WeatherData.empty();
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -33,14 +61,14 @@ class _WeatherPage extends State<WeatherPage>{
       body: new Stack(
         fit: StackFit.expand,
         children: <Widget>[
-          new Image.asset("images/weather_bg.jpg",fit: BoxFit.fitHeight),
+          new Image.asset("images/weather_bg.jpg",fit: BoxFit.fill),
           new Column(
             mainAxisAlignment: MainAxisAlignment.start, //垂直布局
             crossAxisAlignment: CrossAxisAlignment.center,  //水平布局
             children: <Widget>[
               new Container(
                 width: double.infinity,
-                margin: EdgeInsets.only(top: 40.0),
+                margin: EdgeInsets.only(top: 45.0),
                 child: new Text(
                   "邢台市",
                   textAlign: TextAlign.center,
@@ -52,25 +80,25 @@ class _WeatherPage extends State<WeatherPage>{
               ),
               new Container(
                 width: double.infinity,
-                margin: EdgeInsets.only(top: 100.0),
+                margin: EdgeInsets.only(top: 60.0),
                 child: new Column(
                   children: <Widget>[
                     new Text(
-                      "20 °",
+                      weather?.tmp,
                       style: new TextStyle(
                         color: Colors.white,
                         fontSize: 80.0
                       ),
                     ),
                     new Text(
-                      "晴",
+                      weather?.cond,
                       style: new TextStyle(
                         color: Colors.white,
                         fontSize: 45.0,
                       ),
                     ),
                     new Text(
-                      "湿度  80%",
+                      weather?.hum,
                       style: new TextStyle(
                         color: Colors.white,
                         fontSize: 30.0,
