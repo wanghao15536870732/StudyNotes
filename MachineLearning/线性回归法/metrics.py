f0a5fd429d66313c9b809aa8fb2c05188180cc76
wanghao15536870732 <1484290617@qq.com>
2019-05-16 22:24:58 +0800

    :bug: fix bug

--- MachineLearning/线性回归法/metrics.py
+++ MachineLearning/线性回归法/metrics.py
@@ -8,7 +8,7 @@ def accuracy_score(y_true,y_predict):
     return sum(y_true == y_predict) / len(y_true)
 
 def mean_squared_error(y_true,y_predict):
-    """计算y_true和y_predict之间的额MSE"""
+    """计算y_true和y_predict之间的MSE"""
     assert len(y_true) == len(y_predict),\
         "the size of y_true must be equal to the size of y_predict"
     # 平方之和
@@ -20,7 +20,7 @@ def root_mean_squared_error(y_true,y_predict):
     return sqrt(mean_squared_error(y_true,y_predict))
 
 def mean_absolute_error(y_true,y_predict):
-    """计算y_true和y_predict之间的额MAE"""
+    """计算y_true和y_predict之间的MAE"""
     assert len(y_true) == len(y_predict),\
         "the size of y_true must be equal to the size of y_predict"
     # 绝对值之和
